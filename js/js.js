a78e588321a75d432f48e936d5d30dacfa2c1984
eman <eman.elewa@hotmail.com>
2018-07-31 19:34:02 +0200

    finished hesder

---
+++ js/js.js
@@ -0,0 +1,11 @@
+// slide menu
+var myMenu = document.getElementById("x");
+var myButton = document.getElementById("slide_menu");
+
+function z() {
+    if(myMenu.style.display === "none" ){
+        myMenu.style.display = "block";
+    }else{
+        myMenu.style.display = "none";
+    }
+}
